8ccd3ef30e31d374b8162451d9d1dc5d0f683d4b
Surkus <v.surkus@me.com>
2022-01-03 17:46:10 +0200

    Initial commit

---
+++ cs03_paskaita/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace cs03_paskaita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cs03_PASKAITA");
+            #region TEORIJA
+            // Trečia paskaita
+            //
+
+            #endregion
+        }
+
+        public static void Assigment1()
+        {
+
+        }
+
+        public static void Assigment2()
+        {
+
+        }
+
+        public static void Assigment3()
+        {
+
+        }
+
+        public static void Assigment4()
+        {
+
+        }
+
+        public static void Assigment5()
+        {
+
+        }
+    }
+}
+
